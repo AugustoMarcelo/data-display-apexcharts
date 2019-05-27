e300b1c703f2a73628cdf84caf3198d021891582
AugustoMarcelo <mrclgst10@gmail.com>
2019-05-27 09:17:46 -0300

    Subindo projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PopChart from './componentes/PopChart';
+
+function App() {
+    return (
+        <div className="container">
+            <PopChart />
+        </div>
+    );
+}
+
+export default App;
